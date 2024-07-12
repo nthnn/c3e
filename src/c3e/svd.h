fbe6b117a4b2806f6e8c36d982bc20b078bcfb49
Nathanne Isip <nathanneisip@gmail.com>
2024-07-12 22:33:09 +0800

    Single-Value Decomposition header file.

---
+++ src/c3e/svd.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2024 Nathanne Isip
+ * 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ */
+
+#ifndef C3E_SVD_H
+#define C3E_SVD_H
+
+#include <c3e/commons.h>
+
+c3e_svd c3e_svd_init(c3e_matrix* matrix);
+void c3e_svd_free(c3e_svd svd);
+
+#endif
