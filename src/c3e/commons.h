2bc9484c1a430d162096d9e36bdaeadbb70cb232
Nathanne Isip <nathanneisip@gmail.com>
2024-07-12 22:30:30 +0800

    Common definitions and structures on source file.

---
+++ src/c3e/commons.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 Nathanne Isip
+ * 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ */
+
+#ifndef C3E_COMMONS_H
+#define C3E_COMMONS_H
+
+#ifndef PI
+#define PI 3.14159265358979323846
+#endif
+
+#ifndef C3E_32BIT_NUMBER
+typedef double c3e_number;
+#else
+typedef float  c3e_number;
+#endif
+
+typedef struct {
+    int size;
+    c3e_number* data;
+} c3e_vector;
+
+typedef struct {
+    int rows;
+    int cols;
+    c3e_number* data;
+} c3e_matrix;
+
+typedef struct {
+    c3e_matrix* a;
+    c3e_matrix* b;
+} c3e_matrix_tuple;
+
+typedef struct {
+    c3e_matrix* left;
+    c3e_matrix* right;
+    c3e_vector* singular;
+} c3e_svd;
+
+#endif
