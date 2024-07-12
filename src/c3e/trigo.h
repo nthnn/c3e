e4b1674cec6ec3cfc6f7029b7eec62237d4ba6fc
Nathanne Isip <nathanneisip@gmail.com>
2024-07-12 22:30:52 +0800

    Trigonometry related functionality header file.

---
+++ src/c3e/trigo.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2024 Nathanne Isip
+ * 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ */
+
+#ifndef C3E_TRIGO_H
+#define C3E_TRIGO_H
+
+#include <c3e/commons.h>
+
+c3e_number c3e_radian_degrees(c3e_number x);
+c3e_number c3e_gamma(c3e_number x);
+c3e_number c3e_log_gamma(c3e_number x);
+
+#endif
