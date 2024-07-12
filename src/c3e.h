9f42f36baa02d063bf44fea1ad8965984e47b947
Nathanne Isip <nathanneisip@gmail.com>
2024-07-12 22:37:28 +0800

    C3E main header file.

---
+++ src/c3e.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2024 Nathanne Isip
+ * 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ */
+
+#ifndef C3E_H
+#define C3E_H
+
+#include <c3e/commons.h>
+#include <c3e/matrix.h>
+#include <c3e/matrix_tuple.h>
+#include <c3e/svd.h>
+#include <c3e/vector.h>
+
+#endif
