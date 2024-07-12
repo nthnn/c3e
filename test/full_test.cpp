277a41d1292e7f243cc1de5339ecc1f78b40b4f3
Nathanne Isip <nathanneisip@gmail.com>
2024-07-13 04:28:45 +0800

    Improved full test suite with SVD.

--- test/full_test.cpp
+++ test/full_test.cpp
@@ -45,7 +45,7 @@ void test_vector() {
     c3e_vector* vector1 = c3e_vector_init(size);
     c3e_vector* vector2 = c3e_vector_init(size);
 
-    for (size_t i = 0; i < size; i++) {
+    for(size_t i = 0; i < size; i++) {
         c3e_vector_set(vector1, i, (c3e_number)i + 1);
         c3e_vector_set(vector2, i, (c3e_number)(i + 1) * 2);
     }
@@ -87,31 +87,41 @@ void test_vector() {
     c3e_vector_free(result);
 
     c3e_number sum = c3e_vector_sum(vector1);
-    printf("Sum of elements in Vector 1: %f\n", sum);
+    printf("Sum of elements in Vector 1: %f\r\n", sum);
 
     c3e_number norm = c3e_vector_norm(vector1);
-    printf("Norm of Vector 1: %f\n", norm);
+    printf("Norm of Vector 1: %f\r\n", norm);
 
     c3e_number angle = c3e_vector_angle(vector1, vector2);
-    printf("Angle between Vector 1 and Vector 2: %f\n", angle);
+    printf("Angle between Vector 1 and Vector 2: %f\r\n", angle);
 
     c3e_number cross = c3e_vector_cross(vector1, vector2);
-    printf("Cross product of Vector 1 and Vector 2: %f\n", cross);
+    printf("Cross product of Vector 1 and Vector 2: %f\r\n", cross);
 
     c3e_number projection = c3e_vector_projection(vector1, vector2);
-    printf("Projection of Vector 1 on Vector 2: %f\n", projection);
+    printf("Projection of Vector 1 on Vector 2: %f\r\n", projection);
 
     c3e_number dot = c3e_vector_dot(vector1, vector2);
-    printf("Dot product of Vector 1 and Vector 2: %f\n", dot);
+    printf("Dot product of Vector 1 and Vector 2: %f\r\n", dot);
 
-    c3e_number matrix1_data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
-    c3e_number matrix2_data[] = {25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    c3e_number matrix1_data[] = {
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+        10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23,
+        24, 25
+    };
+    c3e_number matrix2_data[] = {
+        25, 24, 23, 22, 21, 20, 19,
+        18, 17, 16, 15, 14, 13, 12,
+        11, 10, 9, 8, 7, 6, 5, 4, 3,
+        2, 1
+    };
     
     c3e_matrix matrix1 = {5, 5, matrix1_data};
     c3e_matrix matrix2 = {5, 5, matrix2_data};
 
     c3e_number dot_cols = c3e_vector_dot_cols(&matrix1, 0, &matrix2, 0);
-    printf("Dot product of columns 0 from matrix1 and matrix2: %f\n", dot_cols);
+    printf("Dot product of columns 0 from matrix1 and matrix2: %f\r\n\r\n", dot_cols);
 
     result = c3e_vector_normalize(vector1);
     printf("Normalization of Vector 1: ");
@@ -157,9 +167,55 @@ void test_vector() {
     c3e_vector_free(vector2);
 }
 
+void test_svd() {
+    c3e_matrix* matrix = c3e_matrix_init(3, 3);
+    c3e_number values[] = {
+        14, 0, 0,
+        21, 175, 0,
+        -14, -70, 35
+    };
+
+    c3e_matrix_set_elements(matrix, values);
+
+    c3e_svd svd = c3e_svd_init(matrix);
+    printf("SVD result:\r\n");
+
+    printf("  - Left (U) matrix:\r\n");
+    for(int i = 0; i < svd.left->rows; i++) {
+        printf("\t");
+
+        for(int j = 0; j < svd.left->cols; j++)
+            printf("%.2f ", c3e_matrix_get_at(svd.left, i, j));
+
+        printf("\r\n");
+    }
+
+    printf("  - Right (V) matrix:\r\n");
+    for(int i = 0; i < svd.right->rows; i++) {
+        printf("\t");
+
+        for(int j = 0; j < svd.right->cols; j++)
+            printf("%.2f ", c3e_matrix_get_at(svd.right, i, j));
+
+        printf("\n");
+    }
+
+    printf("  - Singular (S) values:\r\n\t");
+    for(int i = 0; i < svd.singular->size; i++)
+        printf("%.2f ", c3e_vector_get(svd.singular, i));
+
+    c3e_svd_free(svd);
+    c3e_matrix_free(matrix);
+}
+
 int main() {
     printf("----------------Vector Tests----------------\r\n\r\n");
     test_vector();
+    printf("\r\n");
+
+    printf("------Single-Value Decomposition Tests------\r\n\r\n");
+    test_svd();
+    printf("\r\n");
 
     return 0;
 }
