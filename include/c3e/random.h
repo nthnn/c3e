e4edc8f17b89cd10d451b401bffdcf1a7d8df9ef
Nathanne Isip <nathanneisip@gmail.com>
2024-07-13 19:10:00 +0800

    Copyright header for c3e/random.h

--- include/c3e/random.h
+++ include/c3e/random.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2024 Nathanne Isip
+ * 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ */
+
 #ifndef C3E_RANDOM_H
 #define C3E_RANDOM_H
 
