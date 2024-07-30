7e889790d8d1bc9fdf16e1d946abbba020c2a650
Nathanne Isip <nathanneisip@gmail.com>
2024-07-31 00:37:18 +0800

    Geometrical function implementations for vectors.

--- src/c3e/vector.c
+++ src/c3e/vector.c
@@ -15,12 +15,12 @@
  *    and/or other materials provided with the distribution.
  */
 
+#include <c3e/assert.h>
 #include <c3e/matrix.h>
 #include <c3e/random.h>
 #include <c3e/trigo.h>
 #include <c3e/vector.h>
 
-#include <assert.h>
 #include <math.h>
 #include <stdlib.h>
 
@@ -28,6 +28,13 @@ c3e_vector* c3e_vector_init(size_t size) {
     c3e_vector* vector = (c3e_vector*) malloc(sizeof(c3e_vector));
     vector->size = size;
     vector->data = (c3e_number*) calloc(size, sizeof(c3e_number));
+
+    return vector;
+}
+
+c3e_vector* c3e_vector_create(size_t size, c3e_number* data) {
+    c3e_vector* vector = c3e_vector_init(size);
+    c3e_vector_set_elements(vector, data);
 
     return vector;
 }
@@ -66,7 +73,7 @@ void c3e_vector_set_elements(c3e_vector* vector, c3e_number* values) {
 }
 
 c3e_vector* c3e_vector_add(c3e_vector* vector, c3e_vector* subject) {
-    assert(vector->size == subject->size);
+    c3e_assert(vector->size == subject->size);
 
     c3e_vector* out = c3e_vector_init(vector->size);
     for(int i = 0; i < out->size; i++)
@@ -76,7 +83,7 @@ c3e_vector* c3e_vector_add(c3e_vector* vector, c3e_vector* subject) {
 }
 
 c3e_vector* c3e_vector_sub(c3e_vector* vector, c3e_vector* subject) {
-    assert(vector->size == subject->size);
+    c3e_assert(vector->size == subject->size);
 
     c3e_vector* out = c3e_vector_init(vector->size);
     for(int i = 0; i < out->size; i++)
@@ -85,8 +92,8 @@ c3e_vector* c3e_vector_sub(c3e_vector* vector, c3e_vector* subject) {
     return out;
 }
 
-c3e_vector* c3e_vector_mul(c3e_vector* vector, c3e_vector* subject){
-    assert(vector->size == subject->size);
+c3e_vector* c3e_vector_mul(c3e_vector* vector, c3e_vector* subject) {
+    c3e_assert(vector->size == subject->size);
 
     c3e_vector* out = c3e_vector_init(vector->size);
     for(int i = 0; i < vector->size; i++)
@@ -95,8 +102,8 @@ c3e_vector* c3e_vector_mul(c3e_vector* vector, c3e_vector* subject){
     return out;
 }
 
-c3e_vector* c3e_vector_div(c3e_vector* vector, c3e_vector* subject){
-    assert(vector->size == subject->size);
+c3e_vector* c3e_vector_div(c3e_vector* vector, c3e_vector* subject) {
+    c3e_assert(vector->size == subject->size);
 
     c3e_vector* out = c3e_vector_init(vector->size);
     for(int i = 0; i < vector->size; i++)
@@ -159,7 +166,7 @@ c3e_number c3e_vector_projection(c3e_vector* vector, c3e_vector* subject) {
 }
 
 c3e_number c3e_vector_dot(c3e_vector* vector, c3e_vector* subject) {
-    assert(vector->size == subject->size);
+    c3e_assert(vector->size == subject->size);
 
     c3e_number out = 0.0;
     for(int i = 0; i < vector->size; i++)
@@ -188,7 +195,7 @@ c3e_vector* c3e_vector_normalize(c3e_vector* vector) {
 }
 
 c3e_vector* c3e_vector_transform(c3e_vector* vector, c3e_matrix* matrix) {
-    assert(matrix->cols == vector->size);
+    c3e_assert(matrix->cols == vector->size);
 
     c3e_vector* out = c3e_vector_zeros(matrix->rows);
     for(int i = 0; i < matrix->rows; i++)
@@ -270,4 +277,204 @@ bool c3e_vector_all_close(c3e_vector* vector, c3e_vector* subject) {
             return false;
 
     return true;
+}
+
+c3e_vector* c3e_vector_arc_sin(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = asin(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_arc_sinh(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = asinh(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_sin(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = sin(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_sinh(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = sinh(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_arc_cos(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = acos(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_arc_cosh(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = acosh(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_cos(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = cos(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_cosh(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = cosh(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_arc_tan(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = atan(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_arc_tanh(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = atanh(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_tan(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = tan(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_tanh(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = tanh(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_abs(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = abs(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_log(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = log(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_log10(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = log10(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_log2(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = log2(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_log1p(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = log1p(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_pow(c3e_vector* vector, c3e_number exp) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = pow(vector->data[i], exp);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_rsqrt(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = 1 / sqrt(vector->data[i]);
+
+    return out;
+}
+
+c3e_vector* c3e_vector_sqrt(c3e_vector* vector) {
+    c3e_assert(vector->size == 0);
+
+    c3e_vector* out = c3e_vector_init(vector->size);
+    for(int i = 0; i < vector->size; i++)
+        out->data[i] = sqrt(vector->data[i]);
+
+    return out;
 }
